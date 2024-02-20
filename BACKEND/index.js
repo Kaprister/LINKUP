c12c84221c51bd5f4a8b415eede50ee386818e0a
Kaprister <sushantkapri@gamil.com>
2024-02-20 20:57:28 +0530

    cors config

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -10,7 +10,13 @@ const app = express();
 require("dotenv").config();
 
 
-app.use(cors());
+// app.use(cors());
+app.use(
+	cors({
+		origin: "*",
+		credentials: true,
+	})
+);
 app.use(express.json());
 
 app.use("/api/auth", userRoutes)
